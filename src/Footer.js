14f932bc1fa7a8d1eb06ef777d067f2bfd7abd4e
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-25 16:23:12 -0500

    Footer component added to App.js

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <>
+                <footer className="footer">
+                    <p className="footer__copyright">&copy; 2023 Bryan Garduno Gonzalez</p>
+                </footer>
+            </>
+        )
+    }
+}
+
+
+export default Footer;
