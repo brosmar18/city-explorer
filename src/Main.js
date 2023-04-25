30c1a298efc2711214b7cb0466dc48788bdf65e1
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-25 16:18:46 -0500

    Main component added to App.js

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <>
+                <main className='main'>
+                    <section className='section'>
+                        <h2 className='section__title'>Section Title</h2>
+                        <p className='section__description'>Section Description</p>
+                    </section>
+                </main>
+            </>
+        )
+    }
+}
+
+
+export default Main;
